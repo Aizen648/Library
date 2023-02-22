b86ffdb3080f21390a41bd6b7c55b4954cf62ed9
Aizen648 <you@example.com>
2023-02-22 12:18:39 +0100

    - repository

--- src/main/java/pl/kolo/Library/repository/RentalRepository.java
+++ src/main/java/pl/kolo/Library/repository/RentalRepository.java
@@ -1,6 +1,7 @@
 package pl.kolo.Library.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -8,19 +9,29 @@ import java.util.List;
 
 @Repository
 public class RentalRepository {
-
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public int count(int id){
+    public int count(int id) {
         try {
             return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM rental WHERE idClient=? group by idClient", Integer.class, id);
-        } catch (Exception e){
+        } catch (Exception e) {
             return -1;
         }
     }
-    public List<Integer> borrowed(int id){
-        return jdbcTemplate.queryForList(" SELECT idBook FROM rental WHERE idClient=?",Integer.class, id);
 
+    public List<Integer> borrowed(int id) {
+        return jdbcTemplate.queryForList(" SELECT idBook FROM rental WHERE idClient=?", Integer.class, id);
+    }
+
+    public int add(int idClient, List<Integer> idBooks) {
+        for (Integer i : idBooks) {
+            jdbcTemplate.update("INSERT INTO rental(idClient,idBook) VALUES (?,?)", idClient, i);
+        }
+        return 1;
+    }
+
+    public int delete(int idBook) {
+        return jdbcTemplate.update("DELETE FROM book WHERE idBook=?", idBook);
     }
 }
